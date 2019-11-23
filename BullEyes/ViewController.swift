03c9e183e1e071e469035bbc726e831bbbd4b5f2
Vinh Le <vinh@vertics.co>
2019-11-23 16:08:26 +0200

    feat: add target label, slider and connect with storyboard

--- BullEyes/ViewController.swift
+++ BullEyes/ViewController.swift
@@ -9,12 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 50
+    var targetValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let roundedValue = slider.value.rounded()
+        currentValue = Int(roundedValue)
+        
+        self.startNewRound()
     }
 
-
+    @IBAction func showAlert() {
+        let message: String = "Move to \(currentValue)" +
+        "\n The target value is \(targetLabel.text!)"
+        
+        let alert = UIAlertController(title: "Slider", message: message, preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        self.startNewRound()
+    }
+    
+    @IBAction func moveSlider(_ slider: UISlider) {
+        let roundedValue = slider.value.rounded()
+        print("Current value is \(roundedValue)")
+        currentValue = Int(roundedValue)
+    }
+    
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+        updateLabel()
+    }
+    
+    func updateLabel() {
+        targetLabel.text = String(targetValue)
+    }
 }
 
